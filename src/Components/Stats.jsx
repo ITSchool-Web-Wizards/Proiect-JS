263b2f515f310f70411f69c7dacce758ab0fe66d
Pavaleanu Ciprian <pavaleanuciprian@yahoo.com>
2024-05-13 18:44:21 +0300

    Proiect JS with react

---
+++ src/Components/Stats.jsx
@@ -0,0 +1,31 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  if (numPacked === 0)
+    return (
+      <p className="stats">
+        <em>{`You have ${numItems} ${
+          numItems === 1 ? "item" : "items"
+        } on your list, and you packed none yet`}</em>
+      </p>
+    );
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go!"
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
