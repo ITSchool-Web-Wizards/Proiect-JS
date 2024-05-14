a80eb9798a648aebfbdb8f918ea23f22b242f0e7
Cosmin Ionescu <101202348+CosminIonescu96@users.noreply.github.com>
2024-05-14 10:06:19 +0300

    First version

---
+++ Quizz-app/src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,83 @@
+import React, { useRef } from 'react'
+import { useState } from 'react'
+import './Quiz.css'
+import { data } from '../../assets/data/data'
+
+const Quiz = () => {
+
+    let [index, setIndex] = useState(0);
+    let [question, setQuestion] = useState(data[index]);
+    let [lock, setlock] = useState(false);
+    let [score, setScore] = useState(0);
+
+    let option1 = useRef(null);
+    let option2 = useRef(null);
+    let option3 = useRef(null);
+    let option4 = useRef(null);
+
+    let optionArray = [option1, option2, option3, option4];
+    
+    const checkAnswer = (e, answer) => {
+        if (lock === false) {
+            e.target.classList.add("lock")
+            if (question.answer === answer) {
+                e.target.classList.add("correct");
+                setlock(true);
+                setScore(score + 1);
+            } else {
+                e.target.classList.add("wrong");
+                setlock(true);
+                optionArray[question.answer].current.classList.add("correct");
+            }
+        }
+
+    }
+
+    const refreshQuiz = () => {
+        setIndex(0);
+        setQuestion(data[0]);
+        setlock(false);
+        setScore(0);
+        optionArray.forEach((option) => {
+            option.current.classList.remove("lock");
+            option.current.classList.remove("correct");
+            option.current.classList.remove("wrong");
+        })
+    }
+
+    const nextQuestion = () => {
+        if (index < data.length - 1) {
+            setIndex(index + 1);
+            setQuestion(data[index + 1]);
+            setlock(false);
+            optionArray.forEach((option) => {
+                option.current.classList.remove("lock");
+                option.current.classList.remove("correct");
+                option.current.classList.remove("wrong");
+            })
+        } else {
+            alert("Quiz is over. Your score is " + score + " out of " + data.length);
+        }
+    }
+
+    return (
+        <>
+            <div className='container'>
+                <h1>Quiz App</h1>
+                <hr />
+                <h2>{index + 1}. {question.question}</h2>
+                <ul>
+                    <li ref={option1} onClick={(e) => { checkAnswer(e, 0) }}>{question.options[0]}</li>
+                    <li ref={option2} onClick={(e) => { checkAnswer(e, 1) }}>{question.options[1]}</li>
+                    <li ref={option3} onClick={(e) => { checkAnswer(e, 2) }}>{question.options[2]}</li>
+                    <li ref={option4} onClick={(e) => { checkAnswer(e, 3) }}>{question.options[3]}</li>
+                </ul>
+                <button onClick={nextQuestion} className='button'>Next</button>
+                <div className='index'>{index + 1} of {data.length} questions</div>
+                <button onClick={refreshQuiz} className='refresh'>Refresh Quiz</button>
+            </div>
+        </>
+    )
+}
+
+export default Quiz
---
+++ Quizz-app/src/assets/data/data.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+        question: 'Everything is permitted, Nothing is true.',
+        options: ['Answer1test', 'Answer2test', 'Answer3test', 'Answer4test'],
+        answer: 1
+    },
+    {
+        question: 'The cake is a lie.',
+        options: ['Answer1', 'Answer2', 'Answer3', 'Answer4'],
+        answer: 2
+    },
+    {
+        question: 'The rights of an individual are not as important as the good of the society.',
+        options: ['Answer1', 'Answer2', 'Answer3', 'Answer4'],
+        answer: 2
+    },
+    {
+        question: 'The ends justify the means.',
+        options: ['Answer1', 'Answer2', 'Answer3', 'Answer4'],
+        answer: 3
+    },
+    {
+        question: 'The needs of the many outweigh the needs of the few.',
+        options: ['Answer1', 'Answer2', 'Answer3', 'Answer4'],
+        answer: 0
+    }
+]
