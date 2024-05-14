e852a62c0be1649b745d98650a9b343988ccb0b1
Dragos0403 <109337697+Dragos0403@users.noreply.github.com>
2024-05-14 18:37:42 +0300

    last version

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,110 +1,111 @@
-import './style.css';
+import './style.css'; 
+const cityInput = document.querySelector(".city-input"); 
+const locationButton = document.querySelector(".location-btn"); 
+const searchButton = document.querySelector(".search-btn"); 
+const weatherCardsDiv = document.querySelector(".weather-cards"); 
+const currentWeatherDiv = document.querySelector(".current-weather"); 
+const API_KEY = "9ffdcec4d8b145225eb46b1aef039723"; 
 
-const cityInput = document.querySelector(".city-input");
-const locationButton = document.querySelector(".location-btn");
-const searchButton = document.querySelector(".search-btn");
+const createWeatherCard = (cityName, weatherItem, index) => { 
+    if (index === 0) { 
+        return `
+            <div class="details">
+                <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                <h4>Temperatură : ${Math.round(weatherItem.main.temp - 273.15)}°C </h4>
+                <h4>Vânt: ${Math.round(weatherItem.wind.speed * 3.6).toFixed(2)} km/h</h4>
+                <h4>Umiditate: ${weatherItem.main.humidity}%</h4>
+            </div>
+            <div class="icon">
+                <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                <h4>${weatherItem.weather[0].description}</h4>
+            </div>
+        `;
+    } else { 
+        return ` 
+            <li class="card"> 
+                <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3> 
+                <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png" alt="weather-icon"> 
+                <h4>Temperatură : ${Math.round(weatherItem.main.temp - 273.15)}°C </h4> 
+                <h4>Vânt: ${Math.round(weatherItem.wind.speed * 3.6).toFixed(2)} km/h</h4> 
+                <h4>Umiditate: ${weatherItem.main.humidity}%</h4> 
+            </li>
+        `;
+    } 
+}; 
 
-const weatherCardsDiv = document.querySelector(".weather-cards");
-const currentWeatherDiv = document.querySelector(".current-weather");
+const getWeatherDetails = (cityName, lat, lon) => { 
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`; 
+    fetch(WEATHER_API_URL) 
+        .then(res => res.json()) 
+        .then(data => { 
+            // Clear previous weather data 
+            weatherCardsDiv.innerHTML = ""; 
+            currentWeatherDiv.innerHTML = ""; 
+            
+            // Filter forecast for each day 
+            const uniqueForecastDays = []; 
+            const fiveDaysForecast = data.list.filter(forecast => { 
+                const forecastDate = new Date(forecast.dt_txt).getDate(); 
+                if (!uniqueForecastDays.includes(forecastDate)) { 
+                    uniqueForecastDays.push(forecastDate); 
+                    return true; 
+                } 
+                return false; 
+            }); 
+            
+            // Display weather cards 
+            fiveDaysForecast.forEach((weatherItem, index) => { 
+                if (index === 0) { 
+                    currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index)); 
+                } else { 
+                    weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index)); 
+                } 
+            }); 
+        }) 
+        .catch(() => { 
+            alert("An error occurred while fetching the weather forecast"); 
+        }); 
+}; 
 
-const API_KEY = "9ffdcec4d8b145225eb46b1aef039723";
+const getCityCoordinates = () => { 
+    const cityName = cityInput.value.trim(); 
+    if (!cityName) 
+        return alert("Please enter a city name"); 
+    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`; 
+    fetch(GEOCODING_API_URL) 
+        .then(res => res.json()) 
+        .then(data => { 
+            if (!data.length) 
+                return alert(`No coordinates found for ${cityName}`); 
+            const { name, lat, lon } = data[0]; 
+            getWeatherDetails(name, lat, lon); 
+        }) 
+        .catch(() => { 
+            alert("An error occurred while fetching the coordinates!"); 
+        }); 
+}; 
 
-const createWeatherCard = (cityName, weatherItem, index) => {
-    if (index === 0) {
-        return `<div class="details">
-                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-                    <h4>Temperatură : ${(weatherItem.main.temp - 273.15).toFixed(2)}°C </h4>
-                    <h4>Vânt: ${weatherItem.wind.speed} M/S</h4>
-                    <h4>Umiditate: ${weatherItem.main.humidity}%</h4>
-                </div>
-                <div class="icon">
-                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-                    <h4>${weatherItem.weather[0].description}</h4>
-                </div>`;
-    } else {
-        return ` <li class="card">
-                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
-                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png" alt="weather-icon">
-                    <h4>Temperatură : ${(weatherItem.main.temp - 273.15).toFixed(2)}°C </h4>
-                    <h4>Vânt: ${weatherItem.wind.speed} M/S</h4>
-                    <h4>Umiditate: ${weatherItem.main.humidity}%</h4>
-                </li>`;
-    }
-};
+const getUserCoordinates = () => { 
+    navigator.geolocation.getCurrentPosition( 
+        position => { 
+            const {latitude, longitude} = position.coords; 
+            const REVERSE_GEOCODING_URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`; 
+            //Reverseaza geocodin si preia nume oras din coordonate. 
+            fetch(REVERSE_GEOCODING_URL).then(res => res.json()).then(data => { 
+                const { name} = data[0]; 
+                getWeatherDetails(name, latitude, longitude); 
+            }) 
+            .catch(() => { 
+                alert("An error occurred while fetching the city!"); 
+            }); 
+        }, 
+        error => { 
+            if(error.code === error.PERMISSION_DENIED){ 
+                alert("Cererea de acces a geolocatiei nu a fost acceptata. Te rog sa resetezi permisiunile si sa incerci din nou.") 
+            } 
+        } 
+    ); 
+} 
 
-const getWeatherDetails = (cityName, lat, lon) => {
-    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-
-    fetch(WEATHER_API_URL)
-        .then(res => res.json())
-        .then(data => {
-            // Clear previous weather data
-            weatherCardsDiv.innerHTML = "";
-            currentWeatherDiv.innerHTML = "";
-
-            // Filter forecast for each day
-            const uniqueForecastDays = [];
-            const fiveDaysForecast = data.list.filter(forecast => {
-                const forecastDate = new Date(forecast.dt_txt).getDate();
-                if (!uniqueForecastDays.includes(forecastDate)) {
-                    uniqueForecastDays.push(forecastDate);
-                    return true;
-                }
-                return false;
-            });
-
-            // Display weather cards
-            fiveDaysForecast.forEach((weatherItem, index) => {
-                if (index === 0) {
-                    currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
-                } else {
-                    weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
-                }
-            });
-        })
-        .catch(() => {
-            alert("An error occurred while fetching the weather forecast");
-        });
-};
-
-const getCityCoordinates = () => {
-    const cityName = cityInput.value.trim();
-    if (!cityName) return alert("Please enter a city name");
-
-    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`;
-
-    fetch(GEOCODING_API_URL)
-        .then(res => res.json())
-        .then(data => {
-            if (!data.length) return alert(`No coordinates found for ${cityName}`);
-            const { name, lat, lon } = data[0];
-            getWeatherDetails(name, lat, lon);
-        })
-        .catch(() => {
-            alert("An error occurred while fetching the coordinates!");
-        });
-};
-
-
-
-
-const getUserCoordinates = () =>  {
-    navigator.geolocation.getCurrentPosition(
-        position => {
-            const {latitude, longitude} = position.coords;
-            const REVERSE_GEOCODING_URL =
-     
-        },
-
-        error => {
-            if(error.code === error.PERMISSION_DENIED){
-                alert("Cererea de acces a geolocatiei nu a fost acceptata. Te rog sa resetezi permisiunile si sa incerci din nou.")
-            }
-        }
-    );
-}
-
-locationButton.addEventListener("click", getCityCoordinates);
-searchButton.addEventListener("click", getUserCoordinates);
-
-
+locationButton.addEventListener("click", getUserCoordinates); 
+searchButton.addEventListener("click", getCityCoordinates);
