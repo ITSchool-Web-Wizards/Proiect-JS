3f36c6f62a368108926df5428001de0103261655
Bogdan Man <bogda995@gmail.com>
2024-05-16 12:09:18 +0300

    change

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,12 +1,18 @@
 const canvas = document.getElementById("canvasSignature");
 const context = canvas.getContext("2d");
 const toolIcons = document.querySelectorAll(".tool-icon");
+const colorSwatches = document.querySelectorAll(".color-swatch")
+const pencilTool = document.getElementById("pencilTool");
+const eraserTool = document.getElementById("eraserTool");
+const brushTool = document.getElementById("brushTool");
+const airbrushTool = document.getElementById("airbrushTool");
 
 let isDrawing = false;
-brushWidth = 3;
+// brushWidth = 3;
 //store drawing content
 let drawingHistory = [];
 let selectedTool = "";
+let selectedColor = ""
 let prevMouseX, prevMouseY;
 
 window.addEventListener("load", () => {
@@ -16,6 +22,10 @@ window.addEventListener("load", () => {
 
 const draw = (event) => {
   if (!isDrawing) return;
+
+    // Set the stroke color
+    context.strokeStyle = selectedColor;
+    context.fill = 3;
 
   // Store the scaled coordinates
   const x = event.offsetX * (canvas.width / window.innerWidth);
@@ -28,16 +38,17 @@ const draw = (event) => {
   } else if (selectedTool === "eraserTool") {
     // Use eraser by drawing with transparency
     context.clearRect(x - 5, y - 5, 10, 10); // Clear a small square area centered around (x, y)
-  } else if ((selectedTool === "airbrushTool")) {
+  } else if (selectedTool === "airbrushTool") {
     // set the color and brush style
-    // context.stroke();
+    context.strokeStyle = "##ED1C24";
+    console.log(context.strokeStyle);
     // context.strokeWeigh = brushWidth;
     // find the speed of the mouse movement
     const speed = Math.abs(x - prevMouseX) + Math.abs(y - prevMouseY);
 
     // set minimum radius and spray density of spraypaint brush
-    const minRadius = 10;
-    const sprayDensity = 80;
+    const minRadius = 1;
+    const sprayDensity = 5;
 
     // find radius of the spray paint brush and radius squared
     const r = speed + minRadius;
@@ -52,13 +63,13 @@ const draw = (event) => {
         // Find the lerped X and Y coordinates
         const lerpX = lerp(prevMouseX, x, i / 10);
         const lerpY = lerp(prevMouseY, y, i / 10);
-  
+
         // Draw a bunch of random points within a circle
         for (let j = 0; j < sprayDensity; j++) {
           // Pick a random position within the circle
           const randX = Math.random() * (2 * r) - r;
           const randY = Math.random() * (2 * r) - r;
-  
+
           // Draw the random point
           context.fillRect(lerpX + randX, lerpY + randY, 1, 1);
       }
@@ -119,6 +130,8 @@ function resizeCanvas() {
   drawingHistory = tempHistory;
 }
 
+
+
 // Drawing events
 canvas.addEventListener("mousedown", startDrawing);
 canvas.addEventListener("mousemove", draw);
@@ -139,34 +152,40 @@ toolIcons.forEach((icon) => {
     });
     this.classList.add("selected");
     selectedTool = this.id;
-
-    if (selectedTool !== "eraserTool") {
-      context.globalCompositeOperation = "source-over";
-    }
+    // context.globalCompositeOperation = 'source-over';
   });
 });
 
 // Events for tools
+// Select Color
+colorSwatches.forEach(colorSwatch => {
+    colorSwatch.addEventListener('click', function() {
+        colorSwatches.forEach(swatch => {
+            swatch.classList.remove('selected');
+        });
+        this.classList.add('selected');
+        selectedColor = this.getAttribute('data-color');
+        console.log(selectedColor);
+        // const color = this.getAttribute('data-color');
+        // context.strokeStyle = color;
+    })
+})
 // pencil
-const pencilTool = document.getElementById("pencilTool");
 pencilTool.addEventListener("mousedown", function () {
   selectedTool = "pencilTool";
   context.globalCompositeOperation = "source-over";
 });
 //eraser
-const eraserTool = document.getElementById("eraserTool");
 eraserTool.addEventListener("click", function () {
   selectedTool = "eraserTool";
   context.globalCompositeOperation = "destination-out";
 });
 //brush
-const brushTool = document.getElementById("brushTool");
 brushTool.addEventListener("click", function () {
   selectedTool = "brushTool";
   context.globalCompositeOperation = "multiply";
 });
 //spray
-const airbrushTool = document.getElementById("airbrushTool");
 airbrushTool.addEventListener("click", function () {
   selectedTool = "airbrushTool";
   context.globalCompositeOperation = "source-over";
@@ -179,3 +198,4 @@ airbrushTool.addEventListener("click", function () {
 
 // Brush
 // https://dev.to/ascorbic/a-more-realistic-html-canvas-paint-tool-313b
+// https://library.superhi.com/posts/how-to-paint-with-code-creating-paintbrushes
